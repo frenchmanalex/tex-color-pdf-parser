2e450ac8d350f87cbaee41f0257009cd9e3ad098
frenman <frenchman.alex@gmail.com>
2020-10-17 18:24:07 +0200

    Initial build subtree commit

---
+++ build/precache-manifest.af8eadf9f10d1697240568ea11a003c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfea7d36509405c986b7673d639cf3b8",
+    "url": "/tex-color-pdf-parser/index.html"
+  },
+  {
+    "revision": "45b4a4984dd94cf9dfb2",
+    "url": "/tex-color-pdf-parser/static/css/main.1dea29a9.chunk.css"
+  },
+  {
+    "revision": "c0dade7f5710dd89dfcb",
+    "url": "/tex-color-pdf-parser/static/js/2.cf818773.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tex-color-pdf-parser/static/js/2.cf818773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b4a4984dd94cf9dfb2",
+    "url": "/tex-color-pdf-parser/static/js/main.e7e10303.chunk.js"
+  },
+  {
+    "revision": "340f720351070cf6c941",
+    "url": "/tex-color-pdf-parser/static/js/runtime-main.5956fe25.js"
+  }
+]);
