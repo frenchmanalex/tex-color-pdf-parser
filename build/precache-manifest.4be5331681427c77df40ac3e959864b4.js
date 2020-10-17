2996b4d27c90bb8555b2069ac86703834f478361
frenman <frenchman.alex@gmail.com>
2020-10-17 17:48:01 +0200

    Initial build subtree commit

---
+++ build/precache-manifest.4be5331681427c77df40ac3e959864b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89150c61943cd5f319683347e45d5c47",
+    "url": "/_empty-project/build/index.html"
+  },
+  {
+    "revision": "a9ccb48dba0ac9d0a6d9",
+    "url": "/_empty-project/build/static/css/main.998ea790.chunk.css"
+  },
+  {
+    "revision": "09472d862469dae36526",
+    "url": "/_empty-project/build/static/js/2.59ce4b15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/_empty-project/build/static/js/2.59ce4b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ccb48dba0ac9d0a6d9",
+    "url": "/_empty-project/build/static/js/main.f8c5853d.chunk.js"
+  },
+  {
+    "revision": "4c3c9df593bdf01bb924",
+    "url": "/_empty-project/build/static/js/runtime-main.341264a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/_empty-project/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
